d2a0e2e4d0191d471a747a4ee84a567a5633e806
Anubhav Dhuliya <dhuliya.a@gmail.com>
2023-07-05 11:06:32 +0530

    Chat controller added

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { startSocketServer } from './sockets/chatsHandler.js';
 import http from 'http';
 
 import mongoose from 'mongoose';
+import { ChatsController } from './controllers/chats.controller.js';
 
 const app = express();
 const apiRouter = express.Router();
@@ -17,6 +18,7 @@ const usersController = new UsersController();
 const connectionsController = new ConnectionsController();
 const groupsController = new GroupsController();
 const eventsController = new EventsController();
+const chatsController = new ChatsController();
 
 // Add middleware for JSON parsing and CORS
 apiRouter.use(express.json());
@@ -27,6 +29,7 @@ apiRouter.use('/users', usersController.getRouter());
 apiRouter.use('/connections', connectionsController.getRouter());
 apiRouter.use('/groups', groupsController.getRouter());
 apiRouter.use('/events', eventsController.getRouter());
+apiRouter.use('/chats', chatsController.getRouter());
 
 // Mount the apiRouter with the '/api' prefix
 app.use('/api', apiRouter);
@@ -48,25 +51,36 @@ server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-const username = process.env.DB_USERNAME || 'pachizar_mongodb';
-const password = process.env.DB_PASSWORD || 'Instagram@01';
+const username = process.env.DB_USERNAME || "pachizar_mongodb";
+const password = process.env.DB_PASSWORD || "Instagram@01";
 const encodedPassword = encodeURIComponent(password);
-const clusterName = process.env.DB_CLUSTER || 'cluster0.tljbuxq.mongodb.net';
+const clusterName = process.env.DB_CLUSTER || "cluster0.tljbuxq.mongodb.net";
 const connectionString = `mongodb+srv://${username}:${encodedPassword}@${clusterName}/?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-} as mongoose.ConnectOptions)
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions)
   .then(() => {
     console.log('Connected to MongoDB');
+
+    // Create indexes on different collections
+    const messageIndexPromise = mongoose.connection.db.collection('Messages').createIndex({ eventId: 1 });
+    const userIndexPromise = mongoose.connection.db.collection('Users').createIndex({ username: 1 });
+    // const eventIndexPromise = mongoose.connection.db.collection('Events').createIndex({ createdAt: -1 });
+    // const groupIndexPromise = mongoose.connection.db.collection('Groups').createIndex({ createdAt: -1 });
+    const connectionsIndexPromise = mongoose.connection.db.collection('Connections').createIndexes([{ key:{user1:1} },{key:{user2:1}}]);
+
+    // Wait for all index creation promises to resolve
+    return Promise.all([messageIndexPromise, userIndexPromise, connectionsIndexPromise]);
+  })
+  .then(() => {
+    console.log('Indexes created on different collections');
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
 
+
 const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'Connection error:'));
-
-export { server };
